9ff13173e44f60ab72eab6eafbfe9306d1dca41e
Magy Vinueza <magivt7@icloud.com>
2022-02-07 19:25:13 -0500

    Examen PO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { observable, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 const routes: Routes = [];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'examen';
-}
+  title = 'examen';}
+    
+  var Capturar = function(){
+    let lstNumero = document.getElementsByClassName("numero"),
+        arrayGuardar = [1, 2, 8, 9, 3];         
+    for (var i = 0; i < lstNumero.length; i++) {       
+    
+        }       
+    }
+
